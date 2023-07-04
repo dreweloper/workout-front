203b42929802f00bce0344a7caa0dbd4290cbcf0
dreweloper <dreweloper@gmail.com>
2023-07-04 21:12:50 +0200

    Creo función renderPills. Se renderizan los datos de la API en el HTML correctamente.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // VARIABLES
+
+    const fragment = document.createDocumentFragment();
+    
+    const sectionPills = document.querySelector('#workout-pills');
+
+
 
     // FUNCTIONS
 
@@ -8,12 +15,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const url = 'https://workout-back-dkij.onrender.com/api/v1';
 
         try {
-            
+
             const request = await fetch(url);
 
             const response = await request.json();
 
-            if(response.ok){
+            if (response.ok) {
 
                 const { pills } = response;
 
@@ -26,7 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
             };
 
         } catch (error) {
-            
+
             console.log(error);
 
         };
@@ -34,9 +41,43 @@ document.addEventListener('DOMContentLoaded', () => {
     }; //!FUNC-FETCHAPI
 
 
+    const renderPills = async () => {
+
+        const data = await fetchAPI();
+
+        data.forEach(item => {
+
+            const article = document.createElement('ARTICLE');
+            article.classList.add('closed-pill');
+
+            const header = document.createElement('HEADER');
+
+            const title = document.createElement('H3');
+            title.innerText = item.title;
+
+            const subtitle = document.createElement('P');
+            subtitle.innerText = item.subtitle;
+
+            const anchor = document.createElement('A');
+            anchor.href = '/notebook.html';
+            anchor.innerHTML = `<span class="material-symbols-rounded"> arrow_right_alt </span>`;
+
+            header.append(title, subtitle);
+
+            article.append(header, anchor);
+
+            fragment.append(article);
+
+        });
+
+        sectionPills.append(fragment);
+
+    }; //!FUNC-RENDERPILLS
+
+
     const init = () => {
 
-        fetchAPI();
+        renderPills();
 
     }; //!FUNC-INIT
 
