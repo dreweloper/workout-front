6782ba327ce47923eb8906121300805094c394ab
dreweloper <dreweloper@gmail.com>
2023-07-05 21:12:58 +0200

    Corrijo error en toggle.js y vuelve a funcionar

--- js/toggle.js
+++ js/toggle.js
@@ -1,15 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // VARIABLES
-
-    const maximize = document.querySelector('.toggle-maximize');
-    const minimize = document.querySelector('.toggle-minimize');
-    const card = document.querySelector('#notebook-card')
-
-
-    // EVENTS
-
     document.addEventListener('click', ({ target }) => {
+
+        const maximize = document.querySelector('.toggle-maximize');
+        const minimize = document.querySelector('.toggle-minimize');
+        const card = document.querySelector('#notebook-card');
 
         if(target.matches('.toggle-maximize') || target.matches('.toggle-minimize')){
             maximize.classList.toggle('hidden');
